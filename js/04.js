276429674092b32cd2539bf1594e2d3931511f3d
Edward Martínez <edalmaji87@gmail.com>
2023-03-17 17:07:59 -0500

    se hace ejercicio en js/04.js [200~

---
+++ js/04.js
@@ -0,0 +1,23 @@
+//tipos de datos: STRINGS O CADENAS DE TEXTO
+
+const producto = "Monitor de 20 pulgadas";
+const producto2 = String('Monitor 30 pulgadas');
+const producto3 = new String('Monitor 50 pulgadas');
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
+
+console.log(typeof producto) // muestra en consola que es un string
+console.log(typeof producto2) // muestra en consola que es un string
+console.log(typeof producto3) // muestra en consola que es un objeto
+
+
+//la sintaxis mas utilizada
+
+// la mas utilizada es la primera la detallo a continuación:
+
+// const producto = "Monitor de 20 pulgadas";
+
+// console.log(producto) lo dejo comentado para qeu no me genere error en la consola del navegador
+
