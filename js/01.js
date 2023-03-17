18b98625cfaf5abe9bb8cd6fe43a141c2489e6a5
Edward Martínez <edalmaji87@gmail.com>
2023-03-17 11:56:59 -0500

    se crean 3 archivos 01.js 02.js 03.je en el primero se define las variables con var, en el segundo las diferencias con let y en el tercero las diferencias con const para declarar variables

---
+++ js/01.js
@@ -0,0 +1,32 @@
+// VARIABLES
+
+var producto = 'Audifonos Gamer'; //Inicia una variable y se le asigna un valor
+
+var disponible; // se inicia una variable sin valor
+
+producto = true; //reasignando el valor de la variable
+
+var producto1 = 'computadora',
+    disponible = true,
+    categoria = 'computadoras'; //inicializa multiples variables
+
+
+//REGLAS de las variables
+
+//las variables puedebn tener numeros letras, numeros e incluso algunos caracteres, pero no iniciar con ellos por ejemplo no se puede
+
+// var 1disponible; //esto no se puede hacer ya que la consola nos marca un error
+// var -disponible; //eto tampoco esta permitido aunque el guin bajo no genera error asi este al comienzo
+
+
+
+
+//ESTILOS PARA LAS VARIABLES
+
+var nombre_producto = 'Monitor HD'; //Forma de nombrar una bariable que tiene mas de un nombre para este caso se usa el guin bajo para colocar nombre_prioducto se conoce como "underscore"
+
+var nombreProducto = 'Monitor HD'; //tambien se puede de esta manera separando ela segunda palabrainiciando con letra mayuscula, conocido como "Camelcase"
+
+var NombreProducto = 'Monitor HD'; //o de esta manera que ambas empiecen con mayusculas, conocido como "PascalCase", se utiliza cuando creas una clase
+
+var nombreproducto = 'Monitor HD'; //o de esta manera conocido como "lowercase" 
---
+++ js/03.js
@@ -0,0 +1,36 @@
+//VARIABLES CON const 
+
+const producto = 'Audifonos Gamer'; //Inicia una variable y se le asigna un valor
+
+// const disponible; // NO se puede iniciart una variable sin valor esto genera error en la consola en caso que se necesite iniciar una variable sin valor usar preferiblemente let
+
+// producto = true; //NO permite reasignar un valor
+
+const producto1 = 'computadora',
+    disponible = true,
+    categoria = 'computadoras'; //inicializa multiples variables
+
+
+//REGLAS de las variables
+
+//las variables puedebn tener numeros constras, numeros e incluso algunos caracteres, pero no iniciar con ellos por ejemplo no se puede
+
+// const 1disponible; //esto no se puede hacer ya que la consola nos marca un error
+// const -disponible; //eto tampoco esta permitido aunque el guin bajo no genera error asi este al comienzo
+
+
+
+
+//ESTILOS PARA LAS VARIABLES
+
+const nombre_producto = 'Monitor HD'; //Forma de nombrar una bariable que tiene mas de un nombre para este caso se usa el guin bajo para colocar nombre_prioducto se conoce como "underscore"
+
+const nombreProducto = 'Monitor HD'; //tambien se puede de esta manera separando ela segunda palabrainiciando con constra mayuscula, conocido como "Camelcase"
+
+const NombreProducto = 'Monitor HD'; //de esta manera que ambas empiecen con mayusculas, conocido como "PascalCase", se utiliza cuando creas una clase
+
+const nombreproducto = 'Monitor HD'; //o de esta manera conocido como "lowercase" 
+
+/* EN CONCLUSION: 
+
+NO PERMITE iniciar una variable sin valor y no permite reasignar el valor de una variaable*/
