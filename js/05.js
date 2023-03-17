158ea67cc4e2dc82fa5f1b52f190480eeb53aa32
Edward Martínez <edalmaji87@gmail.com>
2023-03-17 18:23:44 -0500

    se genera un nuevo archivo llamado js/05.js en este se hace ejemplio y ejercicio sobre variables con valor de numeros y asi mismo las operaciones basicas de suma, resta, multiplicación, division y otra cosa llamado modulo que muestra el residuo de una division, podemos mostrarlos en consola tambien con el console.log(numero1 + numero2); y muestra el resultado de la operación

---
+++ js/05.js
@@ -0,0 +1,33 @@
+//NUMEROS Y OPERADORES
+
+const numero1 = 100; //se reflejan en la consola de color morado o azul 
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = -5; //todos los numero los muestra igual sean decimales o negativos no importa
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+//Operaciones
+
+console.log(numero1 + numero2); //me permite validar la suma de dos numeros, en consola me mostrara el numero 300
+
+console.log(numero2 - numero1); //-Nuevamente en consola me muestra el resultado que seria igual al numero 100
+
+console.log(numero2 * numero1); //igual miuestra en consola el resultado de la operación 20000
+
+console.log(numero2 / numero1); //respuesta en consola es 2
+
+
+
+//MODULO, IMPORTANTE forma parte de las entrevistas de trabajo
+
+const numero5 = 7;
+const numero6 = 6;
+
+
+console.log(numero3 % numero4); //lo que hace es una division y muestra el residuo de la misma
+
+//SUENA LOGICO PERO NO SE PUEDEN HACER OPERACIONES ENTRE STRING, GENMNERARA UN EROR LLAMADO NoN = NOT A NUMBER
+
