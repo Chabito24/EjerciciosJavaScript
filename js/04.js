b5f89afed23311bc333934b13ded9f874ecfe0b6
Edward Martínez <edalmaji87@gmail.com>
2023-03-17 17:38:20 -0500

    se realizan 3metodos en el mismo archivo js/04.js metodo .length que muestra la cantidad de caracteres que tiene la frase que escribi en cierto string, el metodo IndexOf que retorna la posicion de una de las palabras escritas en el string y el metodo Includes que me retorna un true o un false validando si la palabra esta o no en la cadena de texto

--- js/04.js
+++ js/04.js
@@ -1,16 +1,16 @@
 //tipos de datos: STRINGS O CADENAS DE TEXTO
 
-const producto = "Monitor de 20 pulgadas";
+const producto = 'Monitor de 20 pulgadas';
 const producto2 = String('Monitor 30 pulgadas');
 const producto3 = new String('Monitor 50 pulgadas');
 
-console.log(producto)
-console.log(producto2)
-console.log(producto3)
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
 
-console.log(typeof producto) // muestra en consola que es un string
-console.log(typeof producto2) // muestra en consola que es un string
-console.log(typeof producto3) // muestra en consola que es un objeto
+console.log(typeof producto); // muestra en consola que es un string
+console.log(typeof producto2); // muestra en consola que es un string
+console.log(typeof producto3); // muestra en consola que es un object
 
 
 //la sintaxis mas utilizada
@@ -20,3 +20,21 @@ console.log(typeof producto3) // muestra en consola que es un objeto
 // const producto = "Monitor de 20 pulgadas";
 
 // console.log(producto) lo dejo comentado para qeu no me genere error en la consola del navegador..
+
+
+//METODOS PARA LOS STRING
+
+const tweet = 'aprendiendo javascript con el curso de desarrollo web completo';
+
+console.log(tweet. length); // este .length me muestra en la consola la cantidad de caracteres que tiene la frase que ubique en el string, en este caso debe mostrar un total de 62 caracteres
+
+//METODO IndexOf (retorna una posicion)
+
+console.log(tweet.indexOf ('javascript')) // el metodo IndexOf, debe ir seguido de parentesis y entre el parentesis la palabra que quiero buscar dentro del string, en este caso javascript, en concola me muestra el numero 12, es decir, que la palabtrra se encuentra escrita despues del caracter numero 12, 
+
+console.log(tweet.indexOf('JavaScript')) // si la palabra esta escrito de forma diferente en este caso con la J y la S mayusculas o cimplemente no encuentra la palabra me va a generar en consola el numero -1, es decir un numero uno megativo
+
+//METODO Includes (retorna un true o false)
+
+console.log(tweet.includes ('javascript')) //como si existe me dice true
+console.log(tweet.includes ('JavaScript')) //al contrario como la palabra esta mal escrita o no existe me genera un false
