338246e4d0d9d373d3dd02a0d6b2a0f17ffe9ae2
Edward Martínez <edalmaji87@gmail.com>
2023-03-17 20:21:17 -0500

    se crea archivo js/06.js en este se realiza ejemplo de el objeto Math sobre una variable llamada resultado y validando con console.log(resultado); los diferentes opciones que podemos hacer para: redondear, generar raices, cambiar un numero a poositivo, tomar el maximo o minimo vaor de un listado, o generar numeros aleatorios

---
+++ js/06.js
@@ -0,0 +1,16 @@
+//el Objeto Math en JS
+
+let resultado;
+
+resultado = Math.PI; //muestra el valor de pi en la consola
+resultado = Math.round(7.8); //redondea al entero mas cercano en la consola deberia mostrar 8
+resultado = Math.ceil(2.1); //redondea el numero siempre hacia arriba
+resultado = Math.floor(2.9); //redondea hacia abajo
+resultado = Math.sqrt(144); //raiz cuadrada
+resultado = Math.abs(-200); //pasa un numero negativo a positivo
+resultado = Math.min( 3, 5, 8, 1, 2, 10, -7 ); //nos retorna el numero mas pequeño dentro de una serie de numeros
+resultado = Math.max( 3, 5, 8, 1, 2, 10, -7 ); //retorna en consola  el numero mas alto dentro del listyado
+resultado = Math.floor( Math.random() * 30 ) // Aqui estoy mezclando o mejor uniendo funciones entre un mat floor a a redondear la multilpicacion de un numero random por 30 y esto si me vba a dar un numero superior a 1 redondeado hacia abajo
+
+console.log(resultado);
+
