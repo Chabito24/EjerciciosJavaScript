a171b82372f49624ef4030b0b72d20be213e8dd8
Edward Martínez <edalmaji87@gmail.com>
2023-04-03 17:14:49 -0500

    se crea archivo 12- congelar o bloquear objetos con freeze y seal, se crea adicional archivo 13- dond muestra como unir dos o mas objetos con un Spread Operator

--- js/12.js
+++ js/12.js
@@ -1,3 +1,8 @@
+//Object Methods
+
+//'use strict'; //si antes de const producto = {..} usamos otra linea de codigo llamada 'use strict'; en la conbsola nos va a generar n error ya que obliga el uso estricto de el manejo de JS donde dira "12.js:11 Uncaught TypeError: Cannot add property imagen, object is not  extensible at 12.js:11:17" que en la linea en este caso la linea de codigo 11 presenta un error 
+
+//object.freeze
 
 const producto = {
     nombreProducto : 'Monitor 30 pulgadas',
@@ -5,5 +10,28 @@ const producto = {
     disponible : true,
 }
 
+Object.freeze(producto); //esto congela el objeto llamado producto, e impide que sea modificado, no permite crear ni eliminar propiedades , al ver la consola vemos que sigue exactamente igual
+
 producto.imagen = 'imagen.jpg';
-console.log(producto); //estas dos ultimas lineas de codigo lo traemos del archivo 10.js de modificar objetos 
+delete producto.precio;
+
+console.log(producto);
+console.log(Object.isFrozen(producto)); //esto nos va a indicar si el objeto esta o no congelado o sellado
+
+
+//SEAL - existe uno similar al freexe pero llamado seal
+
+const productos = {
+    nombreProducto : 'Monitor 50 pulgadas',
+    precio : 700000,
+    disponible : false,
+}
+
+Object.seal(productos); //al igual que freeze no deja crear ni eliminar propiedades del objeto, pero si deja modificar el valor de las propiedades ej: 
+
+productos.precio = 1000000;
+productos.imagen = 'imagen.jpg';
+delete productos.precio;
+
+console.log(productos);
+console.log(Object.isSealed(productos));
---
+++ js/13.js
@@ -0,0 +1,20 @@
+// Unir dos objetos con el Spread Operator
+
+const producto = {
+    nombreProducto : 'Monitor 30 pulgadas',
+    precio : 700000,
+    disponible : true,
+}
+
+const medidas = {
+    peso : '1KG',
+    alto : '1m',
+    ancho : '2m',
+}
+
+// se recomienda no modificar las propiedades de ninguno de los objetos que se van a unir.
+
+const newProduct = { ...producto, ...medidas}; //se crea una nueva variable con una sintaxis de objeto ene ste caso la nueva variable se va a llamar newProduct segido signo de igual y llaves tres puntos el nombre del primer objeto, despues coma, despues nuevamente tres puntos y elo nombre del segundo objeto
+
+console.log(producto); //muestra las propiedades de solo el producto
+console.log(newProduct); //muestra las propiedades de producto mas las propiedades de medidas
