b91e31519aa202cf497dded47f7270f9b3f500f6
Edward Martinez <emartinezj1802@gmail.com>
2025-07-28 09:59:00 -0500

    se crea archivo 27 que contiene1. object literal, object constructor y prototype, video IMPORTANTE

--- js/27.js
+++ js/27.js
@@ -14,12 +14,13 @@ function Producto(categoria, nombre, precio, disponible) {
     this.categoria = categoria;
     this.nombre = nombre;
     this.precio = precio;
-    this.disponible = disponible;
+    this.disponible = true;
 } //formula con la cual se pueden almacenar multiples objetos se asigna function seguido de un nombre para a funcion y entre parentesis los parametros que va a contener los objetos
 
-const producto2 = new Producto('caninos', 'pechera', 55000, true);
-const producto3 = new Producto('caninos', 'pelota', 15000, false);
-const producto4 = new Producto('felinos', 'rascador', 150000, true);
+const producto2 = new Producto('caninos', 'pechera', 55000);
+const producto3 = new Producto('caninos', 'pelota', 15000);
+const producto4 = new Producto('felinos', 'rascador', 150000);
+const producto5 = new Producto();
 
 console.log("objetos construidos con object literal");
 console.log(producto);
@@ -27,3 +28,21 @@ console.log("objetos construidos con object constructor");
 console.log(producto2);
 console.log(producto3);
 console.log(producto4.disponible);// aqui use una propiedad del objeto para traer en consola solo la disponibilidad del producto
+console.log(producto5); //imprime en consola un objeto vacio, bueno ssolo imprime el true por que esta definido se vaor directo a cada producto nuevo.
+
+
+//PROTOTYPES
+
+function FormatearProducto(producto) {
+    return `function: El producto ${producto.nombre} tiene un precio de $ ${producto.precio}`;
+
+}
+
+console.log( FormatearProducto(producto4)); // el problema de crear este tipo de unciones es que me va a ocar crear una por cada objeto, es decir, por ejemplo si tengoo tro objeto llamado cliente, otro llamado proveedores, otro llamado bodegas, otro llamado gastos, por cada uno me toca crear una funcion, por ello se creo algo llamado PROTOTYPES.
+
+// Prototype noes permite crear funcio0nes que solo se utilizan en un objeto en especifico.
+
+Producto.prototype.FormatearProducto = function() {
+    return `Prototype: El producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+}
+console.log(producto3.FormatearProducto());
