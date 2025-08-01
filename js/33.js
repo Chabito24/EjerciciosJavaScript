c8e660154efcc10d1ecc4fc97de3837ddac11681
Edward Martinez <emartinezj1802@gmail.com>
2025-08-01 17:00:18 -0500

    se crea archivo 33.js es igual un async await pero valios que se pueden ejecutar como dependientes o independientes

---
+++ js/33.js
@@ -0,0 +1,56 @@
+// ejecutar dos o mas - Async / Await 
+
+//IMPORTANTE PREGUNTA ENTREVISTA
+
+function descargaNuevosClientes() {
+    return new Promise( resolve => {
+        console.log('Descargando clientes......Espere.'); 
+
+        setTimeout( () => {
+            resolve('Los clientes fueron descargados');
+        }, 5000);
+    }); 
+}
+
+function descargaNuevosPedidos() {
+    return new Promise( resolve => {
+        console.log('Descargando pedidos......Espere.'); 
+
+        setTimeout( () => {
+            resolve('Los pedidos fueron descargados');
+        }, 3000);
+    }); 
+}
+
+async function app() { 
+    try {
+        const resultado = await Promise.all([descargaNuevosClientes(), descargaNuevosPedidos()]); //De est amanera obtenemos un arreglo por cada funcion
+        console.log(resultado[0]); //imprimimos el arreglo por su indice en este caso indice [0] y abajo indice [1]
+        console.log(resultado[1]);
+    } catch (error) {
+        console.log(error)     
+    }
+    
+} 
+
+app(); 
+
+// clientes tarda 5 seg y pedidos 3 seg, pero los 3 segundos de los pedidos eta dentro del rango de los 5 de los clientes asi podemos llamar multiples async away sin que uno sea dependiente de otro para que sean dependientes simplemente reemplazamos el await de la siguiente manera
+
+/*
+
+async function app() { 
+    try {
+        const clientes = await descargaNuevosClientes();
+        const pedidos = await descargaNuevosPedidos();
+        console.log(clientes);
+        console.log(pedidos);
+    } catch (error) {
+        console.log(error)     
+    }
+    
+} 
+
+*/
+
+// de esta manera son dependientes y una nos e ejecuta hasta que la anterior se ejecute.
